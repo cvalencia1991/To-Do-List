ecbbd99057ef8b6b39e1787b45dd434bf1f63218
cvalencia1991 <cesar4a6z@gmail.com>
2022-09-28 15:07:47 -0500

    Fix files with linters

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,35 +1,39 @@
-import "./styles.scss";
-import "./refresh.png";
-import "./enter.png";
-import "./menu.png";
+/* eslint-disable no-unused-vars */
+/* eslint-disable prefer-destructuring */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable guard-for-in */
+import './styles.scss';
+import './refresh.png';
+import './enter.png';
+import './menu.png';
 
 const task = [
-    {
-        description:'wash the dishes',
-        completed: false,
-        index:'1',
-    },
-    {
-        description:'wash the dishes',
-        completed : false,
-        index:'2',
-    },
-    {
-        description:'wash the dishes',
-        completed : false,
-        index:'3',
-    }
+  {
+    description: 'wash the dishes',
+    completed: false,
+    index: '1',
+  },
+  {
+    description: 'wash the dishes',
+    completed: false,
+    index: '2',
+  },
+  {
+    description: 'wash the dishes',
+    completed: false,
+    index: '3',
+  },
 
 ];
 
-let listask = () =>{
-    for(const taska in task){
-        const description = task[taska].description;
-        const completed = task[taska].completed;
-        const index = task[taska].index;
-        const listask = document.getElementById('listtask');
-        const addtask = document.createElement('div');
-        addtask.innerHTML =`
+const listask = () => {
+  for (const taska in task) {
+    const description = task[taska].description;
+    const completed = task[taska].completed;
+    const index = task[taska].index;
+    const listask = document.getElementById('listtask');
+    const addtask = document.createElement('div');
+    addtask.innerHTML = `
         <li  class="itemtask" id='list -${index}'>
         <div>
             <input type="checkbox" id="cbox1" value="first_checkbox" aria-label="alfa">
@@ -40,8 +44,7 @@ let listask = () =>{
         </div>
         </li>
         `;
-        listask.appendChild(addtask);
-    }
-}
+    listask.appendChild(addtask);
+  }
+};
 listask();
-
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,45 +1,45 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-module.exports= {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname,'./dist'),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
-    ],
-    module: {
-        rules: [
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        use: [
           {
-            test: /\.s[ac]ss$/i,
-            use: ['style-loader', 'css-loader',"sass-loader"],
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              output: 'imgs/',
+              useRelativePath: true,
+            },
           },
-           {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-           use: [
-            {
-                loader: 'file-loader',
-                options:{
-                 name: '[name].[ext]',
-                 output: 'imgs/',
-                 useRelativePath: true,
-                }
-            }
-           ]
-        }
         ],
       },
-      devServer: {
-        static: './dist',
-        port: 8020,
-      },
-      optimization: {
-        runtimeChunk: 'single',
-      },
-
-}
+    ],
+  },
+  devServer: {
+    static: './dist',
+    port: 8020,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
