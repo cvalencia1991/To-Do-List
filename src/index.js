e7a41f2bb0e26ad27135127750cb2a4b82ced6ba
cvalencia1991 <cesar4a6z@gmail.com>
2022-10-01 16:06:49 -0500

    Edit files with linters

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-globals */
 import './styles.scss';
 import './imgs/enter.png';
 import './imgs/menu.png';
@@ -5,48 +6,46 @@ import './imgs/refresh.png';
 import Task from './modules/constructor.js';
 import addtasklocalstorage from './modules/localstorage.js';
 
-
 document.getElementById('clear').addEventListener('click', () => {
   localStorage.clear();
   location.reload();
 });
 
-document.addEventListener('DOMContentLoaded',(tasks)=>{
-  if(localStorage.getItem('tasks') == null){
-    return;
-  }else{
+document.addEventListener('DOMContentLoaded', (tasks) => {
+  if (localStorage.getItem('tasks') == null) {
+  } else {
     tasks = JSON.parse(localStorage.getItem('tasks'));
-    tasks.forEach(description=>{
+    tasks.forEach((description) => {
       const ui = new Methods();
       ui.createelement(description);
-    })
+    });
   }
 });
 
-listtask.addEventListener('click',(e)=>{
-  if(e.target.name='deletetask'){
+const listtask = document.getElementById('listtask');
+
+listtask.addEventListener('click', (e) => {
+  if (e.target.name = 'deletetask') {
     const Deleteid = parseInt(e.target.getAttribute('id'));
     const tasks = JSON.parse(localStorage.getItem('tasks'));
-    const tasks2 = tasks.filter(task =>{ return task.id !== Deleteid} );
+    const tasks2 = tasks.filter((task) => task.id !== Deleteid);
     localStorage.setItem('tasks', JSON.stringify(tasks2));
-    const element =document.getElementById(`${Deleteid}`)
-    console.log(element);
+    const element = document.getElementById(`${Deleteid}`);
     element.parentElement.parentElement.remove();
   }
-
 });
 
-export class Methods {
+class Methods {
   createelement(taskinfo) {
     let id;
     const check = 'checked';
     const uncheck = '';
     const listtask = document.getElementById('listtask');
     const tasks4 = JSON.parse(localStorage.getItem('tasks'));
-    if(tasks4 === null){
-      id = 0
-    }else{
-      id= tasks4.pop().id + 1;
+    if (tasks4 === null) {
+      id = 0;
+    } else {
+      id = tasks4.pop().id + 1;
     }
     const complete = taskinfo.complete ? check : uncheck;
     listtask.style.border = '1px solid #888';
@@ -61,66 +60,27 @@ export class Methods {
               </div>
             </li>`;
     listtask.insertAdjacentHTML('afterbegin', text);
-  /*   localStorage.setItem('tasks', JSON.stringify()); */
     this.resetform();
   }
+
   resetform =() => {
     document.getElementById('form').reset();
   }
 }
 
 const enter = document.getElementById('enter');
-let counter ;
+let counter;
 enter.addEventListener('click', (event) => {
   event.preventDefault();
   const description = document.getElementById('listtext').value;
   const tasks5 = JSON.parse(localStorage.getItem('tasks'));
-  if(localStorage.getItem('tasks')== null){
-    counter =0;
-  }else{
-   counter= tasks5.pop().id + 1;
+  if (localStorage.getItem('tasks') == null) {
+    counter = 0;
+  } else {
+    counter = tasks5.pop().id + 1;
   }
   const taskinfo = new Task(description, counter, false);
   const ui = new Methods();
   ui.createelement(taskinfo);
   addtasklocalstorage(taskinfo);
 });
-
-/* let edittask = document.getElementById('edittask')
-
-edittask.addEventListener('click',(e)=>{
-  alert('something');
-})
-
-
- const edittask = document.getElementById('edittask');
-
-edittask.addEventListener('click',(e)=>{
-        console.log(e.target);
-})
- const algo = document.getElementsByName('deletetask');
-console.log(algo);  */
-
-
-/* const completetoDo = (element) => {
-  element.classlist.toggle(check);
-  element.classlist.toggle(uncheck);
-  LIST[element.id].completed = !LIST[element.id].completed;
-};
-
-const removetoDo = (element) => {
-  element.parentElement.parentElement.remove();
-  LIST[element.id].completed = true;
-};
-
-listtask.addEventListener('click', (event) => {
-  const element = event.target;
-  const elementJob = element.attributes.job.value;
-  if (elementJob === 'complete') {
-    completetoDo(element);
-  } else if (elementJob === 'delete') {
-    removetoDo(element);
-  }
-});
-const hst = document.getElementById('listtask');
-*/
